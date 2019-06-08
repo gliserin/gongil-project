ec386cf686a7581248f52de7de8e484b832bcb60
Dongho Oh <graystone117@gmail.com>
2019-06-08 19:15:12 +0900

    MainActivity

--- app/src/main/java/com/odhs/gongilproject/activity/MainActivity.kt
+++ app/src/main/java/com/odhs/gongilproject/activity/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.odhs.gongilproject
+package com.odhs.gongilproject.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odhs.gongilproject.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
